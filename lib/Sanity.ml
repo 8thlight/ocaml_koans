74a098ec7ad74182bda2d3a6db04de4f4295cd89
Eric Smith <esmith@8thlight.com>
2018-12-18 17:44:00 -0600

    Start using alcostest instead
    Alcotest works far better for koans. It gives clean output, shows progress and
    stops after the first failure. Sequential order is also guaranteed. Honestly
    it's like...99% of what you need.

--- lib/Sanity.ml
+++ lib/Sanity.ml
@@ -1,2 +1,1 @@
-let sanity () =
-  print_endline "Hello, World!"
+let sanity () = 3
--- test/test.ml
+++ test/test.ml
@@ -1,6 +1,15 @@
-open! Base
-open! Stdio
 
-let%expect_test "Sanity" =
-  Lib.Sanity.sanity ();
-  [%expect{| Hello, World! |}]
+let my_test () =
+  Alcotest.(check int) "sanity is 3" 3 (Lib.Sanity.sanity ())
+
+let my_test_2 () =
+  Alcotest.(check int) "sanity is also 4" 3 (Lib.Sanity.sanity ())
+
+
+let assert_set = [
+  "Asserts", `Quick, my_test;
+  "More Asserts", `Quick, my_test_2;
+]
+
+let () =
+  Alcotest.run "Asserts" ["assert_set", assert_set]
