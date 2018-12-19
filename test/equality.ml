a1acce647c9237c65df4c6612a8ef88812079069
Eric Smith <esmith@8thlight.com>
2018-12-19 07:00:13 -0600

    Create an equality commit
    The first koans are created for equality.

---
+++ test/equality.ml
@@ -0,0 +1,34 @@
+let __ = false
+let ___ = 1.0
+let ____ = 0
+
+let basic_equality_test () =
+  Alcotest.(check bool) "Checking for equal objects" true (__ = true)
+
+let identity_test () =
+  Alcotest.(check bool) "Bools have the same identity" true (__ == true)
+
+let comparing_integers () =
+  Alcotest.(check bool) "Equal" __ (0 = 0);
+  Alcotest.(check bool) "Identical" __ (0 == 0);
+  Alcotest.(check bool) "Not equal" __ (1 <> 0);
+  Alcotest.(check bool) "Not identical" __ (1 != 0)
+
+let comparing_floats_may_not () =
+  Alcotest.(check bool) "Equal" true (___ = 0.0);
+  Alcotest.(check bool) "Identical" false (___ == 0.0);
+  Alcotest.(check bool) "Not equal" __ (1 <> 0);
+  Alcotest.(check bool) "Not identical" __ (1 != 0)
+
+(* Add enums, strings, and what is nil *)
+(* And operators *)
+
+let equality_set = [
+  "Check Basic Equality", `Quick, basic_equality_test;
+  "Check Identity", `Quick, identity_test;
+  "Compare Integers", `Quick, comparing_integers;
+  "Compare Floats", `Quick, comparing_floats_may_not;
+]
+
+let () =
+  Alcotest.run "Equality" ["equality_set", equality_set]
