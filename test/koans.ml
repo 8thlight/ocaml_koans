52a57ead96607b90c53e9736b00acf98eb3decc6
Alexis Jennings <ajennings@activecampaign.com>
2018-12-19 16:50:20 -0600

    ocaml string tests

--- test/koans.ml
+++ test/koans.ml
@@ -1,3 +1,6 @@
 let () =
   Alcotest.run "Koans"
-    ["equality_set", Equality.equality_set]
+    [
+      (* "equality_set", Equality.equality_set; *)
+      "strings_set", Strings.strings_set
+    ]
