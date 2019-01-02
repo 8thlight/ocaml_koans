39e112c72ca0b1bee513672665ffc22339906109
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2019-01-02 14:36:05 -0600

    Make fail

--- test/list_set.ml
+++ test/list_set.ml
@@ -13,28 +13,28 @@ let get_int_value_at arr indx =
 
 
 let identity_test () =
-  Alcotest.(check bool) "Lists have the same identity" true (
+  Alcotest.(check bool) "Lists have the same identity" __ (
     phys_equal [] []
   )
 
 let appending_to_list () =
   Alcotest.(check string) "First Element" "foo"
     (
-      get_string_value_at (["foo"; "boo";]@["loo";]) 0
+      get_string_value_at ([____; "boo";]@["loo";]) 0
     );
 
   Alcotest.(check string) "First Element" "boo"
     (
-      get_string_value_at (["foo"; "boo";]@["loo";]) 1
+      get_string_value_at (["foo"; ____;]@["loo";]) 1
     );
 
   Alcotest.(check string) "First Element" "loo"
     (
-      get_string_value_at (["foo"; "boo";]@["loo";]) 2
+      get_string_value_at (["foo"; "boo";]@[____;]) 2
     )
 
 let length_of_list () =
-  Alcotest.(check int) "Length Should Be 0" 0 (
+  Alcotest.(check int) "Length Should Be 0" ___ (
     List.length []
   )
 
@@ -44,32 +44,27 @@ let map_filter_reduce () =
       phys_equal (List.map ~f:(fun x -> x*x) []) []
     );
 
-  Alcotest.(check bool) "Mapping over a list of integers - first element" true
+  Alcotest.(check bool) "Mapping over a list of integers" true
     (
-      List.equal ~equal:phys_equal (List.map ~f:(fun x -> x*x) [1;2;3]) [1;4;9]
+      List.equal ~equal:phys_equal (List.map ~f:(fun x -> x*x) [1;2;3]) [___;___;___]
     );
 
   Alcotest.(check int) "Mapping over a list of integers - second element" 4
     (
-      get_int_value_at (List.map ~f:(fun x -> x*x) [1;2;3]) 1
-    );
-
-  Alcotest.(check int) "Mapping over a list of integers - third element" 9
-    (
-      get_int_value_at (List.map ~f:(fun x -> x*x) [1;2;3]) 2
+      get_int_value_at (List.map ~f:(fun x -> x*___) [1;2;3]) 1
     );
 
   Alcotest.(check bool) "Filtering a list of integers" true
     (
-      List.equal ~equal:phys_equal (List.filter ~f:(fun x -> phys_equal x 3) [1;2;3]) [3;]
+      List.equal ~equal:phys_equal (List.filter ~f:(fun x -> phys_equal x 3) [1;2;3]) [___]
     );
 
   Alcotest.(check bool) "Filtering a list where no entries match" true
     (
-      List.equal ~equal:phys_equal (List.filter ~f:(fun x -> phys_equal x 20) [1;2;3]) []
+      List.equal ~equal:phys_equal (List.filter ~f:(fun x -> phys_equal x ___) [-1;2;3]) []
     );
 
-  Alcotest.(check int) "Summing an list using reduce" 6
+  Alcotest.(check int) "Summing a list using reduce" 6
     (
       Option.value ~default:0 (
         List.reduce ~f:(+) [1; 2; 3;]
@@ -79,14 +74,14 @@ let map_filter_reduce () =
   Alcotest.(check string) "Concatenating list of strings using reduce" "mylistreduced"
     (
       Option.value ~default:"" (
-        List.reduce ~f:(fun acc indx -> acc ^ indx) ["my"; "list"; "reduced";]
+        List.reduce ~f:(fun acc indx -> acc ^ indx) [____; "list"; "reduced";]
       )
     );
 
   Alcotest.(check string) "Reducing a string list with a default value" "defaultlistreduced"
     (
       let list = ["list"; "reduced"] in
-      let default = "default" in
+      let default = ____ in
       Option.value ~default:"" (
         List.reduce ~f:(fun acc indx -> acc ^ indx) ([default;]@list)
       )
